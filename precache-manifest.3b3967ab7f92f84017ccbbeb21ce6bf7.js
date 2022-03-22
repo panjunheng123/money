e38c0213d02bce27003feed8d99f18a1c9ab5b21
PandA <1554296563@qq.com>
2022-03-22 20:49:04 +0800

    deploy

---
+++ precache-manifest.3b3967ab7f92f84017ccbbeb21ce6bf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e30b43035a9ab1db41",
+    "url": "/money/css/app.0a6241b5.css"
+  },
+  {
+    "revision": "9b82e34a761f450dd635799023331888",
+    "url": "/money/img/cat2.9b82e34a.jpg"
+  },
+  {
+    "revision": "5dc76bb2907c8f30fa0b9356e8f1de57",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "34e30b43035a9ab1db41",
+    "url": "/money/js/app.fc06b62d.js"
+  },
+  {
+    "revision": "b431c71f4031c806864d",
+    "url": "/money/js/chunk-vendors.64e4b428.js"
+  },
+  {
+    "revision": "a611b54da43cd3710665f3f4e23c2050",
+    "url": "/money/manifest.json"
+  },
+  {
+    "revision": "73ea2e85a60b442cbd9450332f8fd58b",
+    "url": "/money/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money/robots.txt"
+  }
+]);
